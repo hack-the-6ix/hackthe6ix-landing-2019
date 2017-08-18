33e7e10390198b0d06cb3ac835c2040257e2b7c4
Dennis Pacewicz <dennis.pacewicz@gmail.com>
2017-08-18 17:06:42 -0400

    re-enable application page

--- src/router/index.js
+++ src/router/index.js
@@ -32,11 +32,11 @@ export default new Router({
           name: 'Hello',
           component: Hello
         },
-        // {
-        //   path: '/apply',
-        //   name: 'Apply',
-        //   component: Apply
-        // },
+        {
+          path: '/apply',
+          name: 'Apply',
+          component: Apply
+        },
         {
           path: '/privacy',
           name: 'Privacy',
