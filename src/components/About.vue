0259dbad77973197a3173dba0f47ed03c2fc96cf
nickjiang119 <nickjiang119@hotmail.com>
2018-07-13 02:36:17 -0400

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -10,7 +10,7 @@
 	      <h1>About</h1>
         <div class="items">
           <div class="card">
-            <p>Hack the 6ix is Toronto’s largest summer hackathon. Every year, we bring together 400 of the best and brightest hackers from across North America for 36 hours of coding, binging, and jam-packed fun.</p>
+            <p>Hack the 6ix is Toronto’s largest summer hackathon. Every year, we bring together 400 of the best and brightest hackers from across North America for 36 hours of coding, designing, and jam-packed fun.</p>
   
             <p>We’ll provide you with all the resources, mentorship, and food to do your thing; all you need to bring is your creativity and ideas. Hack the 6ix is also a gathering of some of the top companies in North America. Whether you’re looking for start-up or corporate opportunities, come ready to flex your networking game! Hack the 6ix is open to hackers of all levels: no matter if you’re a beginner or veteran, it’s a weekend not to miss!</p>
           </div>
