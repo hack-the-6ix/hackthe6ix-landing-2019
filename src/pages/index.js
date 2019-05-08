9edbb3cb2e82639bbac280dc5376de4148bf0da7
fpunny <frederic.pun@gmail.com>
2019-05-08 00:38:39 -0400

    Changed sections.js to layouts.js to better reflect role

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import * as Sections from './sections';
+import * as LayoutPages from './layouts';
 import * as Layouts from '../layouts';
 
 // Automate all routing based on component metadata + filesystem
@@ -6,7 +6,7 @@ export const routes = (
   Object.values(Layouts).map(layout => ({
     path: layout.path,
     component: layout,
-    children: Object.values(Sections[layout.name]).map(component => {
+    children: Object.values(LayoutPages[layout.name]).map(component => {
       const { name, path = '' } = component;
       return { path, name, component };
     })
