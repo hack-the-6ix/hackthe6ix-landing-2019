6432f038cba4ffb936c59a4616b84d7a84d940c4
Dennis Pacewicz <dennis.pacewicz@shopify.com>
2019-08-12 12:48:39 -0400

    add stub page which redirects back home from /apply

---
+++ src/pages/CardLayout/ApplyStub.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    hewwo
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ApplyStub',
+  path: '/apply',
+  components: {
+  },
+  data() {
+    return {}
+  },
+  created() {
+    this.$router.push('/')
+  },
+};
+</script>
+
+<style lang="scss">
+@import '~@styles/_mixins.scss';
+@import '~@styles/_variables.scss';
+</style>
--- src/pages/CardLayout/index.js
+++ src/pages/CardLayout/index.js
@@ -1,10 +1,12 @@
 import Apply from './Apply';
+import ApplyStub from './ApplyStub';
 import Dashboard from './Dashboard';
 import Subscribe from './Subscribe';
 import Unsubscribe from './Unsubscribe';
 
 export const CardLayout = {
   Apply,
+  ApplyStub,
   Dashboard,
   Subscribe,
   Unsubscribe,
