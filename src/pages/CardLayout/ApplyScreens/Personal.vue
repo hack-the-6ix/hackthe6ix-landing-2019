51441dd55e3331e6ca907c74795eb0b978cee6b5
Henry T <henry@henrytu.me>
2020-06-28 01:05:53 -0400

    formProvider

--- src/pages/CardLayout/ApplyScreens/Personal.vue
+++ src/pages/CardLayout/ApplyScreens/Personal.vue
@@ -6,7 +6,6 @@
       name="first_name"
       placeholder="e.g. John"
       label="First Name"
-      v-model="first_name_"
       required
     />
     <Input
@@ -14,7 +13,6 @@
       name="last_name"
       placeholder="e.g. Doe"
       label="Last Name"
-      v-model="last_name_"
       required
     />
     <Input
@@ -23,7 +21,6 @@
       type="email"
       placeholder="e.g. john@hackthe6ix.com"
       label="Email"
-      v-model="email_"
       :state="typeof emailError === 'string' ? !Boolean(emailError) : undefined"
       :errorMsg="emailError"
       required
@@ -32,13 +29,11 @@
       class="apply__input"
       name="acceptance"
       label="I allow Hack The 6ix to send me emails containing information from the event sponsors."
-      v-model="casl_acceptance_"
     />
     <Select
       class="apply__input"
       label="Gender"
       name="gender"
-      v-model="gender_"
       :options="genders"
       :blur="blur"
       required
@@ -47,7 +42,6 @@
       class="apply__input"
       label="Timezone"
       name="timezone"
-      v-model="timezone_"
       :options="timezones"
       :blur="blur"
       required
@@ -57,7 +51,6 @@
       label="Country"
       name="country"
       description="Note: If you are outside of Canada, we will not be shipping Hack the 6ix swag to your address."
-      v-model="country_"
       :options="countries"
       :blur="blur"
       required
@@ -68,27 +61,23 @@
         name="address_line_1"
         placeholder="123 Hack the 6ix Blvd"
         label="Address Line 1"
-        v-model="address_line_1_"
       />
       <Input
         class="apply__input"
         name="address_line_2"
         placeholder="Unit 6"
         label="Address Line 2"
-        v-model="address_line_2_"
       />
       <Input
         class="apply__input"
         name="city"
         placeholder="Toronto"
         label="City"
-        v-model="city_"
       />
       <Select
         class="apply__input"
         label="Province"
         name="province"
-        v-model="province_"
         :options="provinces"
         :blur="blur"
       />
@@ -97,7 +86,6 @@
         name="postal_code"
         placeholder="M5S 2E4"
         label="Postal Code"
-        v-model="postal_code_"
       />
     </div>
     Some timezone debug stuff: {{ timezone_ || 'No timezone data :(' }}
@@ -108,6 +96,8 @@
 import Checkbox from '@hackthe6ix/vue-ui/Checkbox';
 import Select from '@hackthe6ix/vue-ui/Select';
 import Input from '@hackthe6ix/vue-ui/Input';
+import formProvider from '@hackthe6ix/vue-ui/utils/mixins/formProvider';
+
 import {
   GENDER_ENUM,
   HAS_EMAIL,
@@ -119,13 +109,29 @@ import {validate, query} from '@utils';
 
 export default {
   name: 'Personal',
+  mixins: [
+    formProvider({
+      first_name: '',
+      last_name: '',
+      email: '',
+      acceptance: false,
+      gender: '',
+      timezone: '',
+      country: '',
+      address_line_1: '',
+      address_line_2: '',
+      city: '',
+      province: '',
+      postal_code: '',
+    }),
+  ],
   components: {
     Input,
     Select,
     Checkbox,
   },
   props: {
-    first_name: String,
+    first_name_: String,
     last_name: String,
     email: String,
     casl_acceptance: Boolean,
@@ -142,23 +148,10 @@ export default {
   },
   data() {
     return {
-      first_name_: this.first_name,
-      last_name_: this.last_name,
-      email_: this.email,
-      casl_acceptance_: this.casl_acceptance,
-      gender_: this.gender,
       genders: Object.values(GENDER_ENUM),
       countries: Object.values(COUNTRIES_ENUM),
       provinces: PROVINCES_ENUM,
-      timezone_: this.timezone,
       timezones: TIMEZONES,
-      emailError: undefined,
-      address_line_1_: this.address_line_1,
-      address_line_2_: this.address_line_2,
-      city_: this.city,
-      province_: this.province,
-      postal_code_: this.postal_code,
-      country_: this.country,
     };
   },
   updated() {
@@ -197,46 +190,6 @@ export default {
       }
       this.emailError = 'Please provide a valid email';
       return false;
-    },
-  },
-  watch: {
-    first_name_(val) {
-      this.$emit('update:first_name', val);
-    },
-    last_name_(val) {
-      this.$emit('update:last_name', val);
-    },
-    email_(val) {
-      this.$emit('update:email', val);
-    },
-    casl_acceptance_(val) {
-      this.$emit('update:casl_acceptance', val);
-    },
-    gender_(val) {
-      this.$emit('update:gender', val);
-    },
-    timezone_(val) {
-      this.$emit('update:timezone', val);
-    },
-    country_(val) {
-      this.$emit('update:country', val);
-    },
-    address_line_1_(val) {
-      this.$emit('update:address_line_1', val);
-    },
-    address_line_2_(val) {
-      this.$emit('update:address_line_1', val);
-    },
-    city_(val) {
-      this.$emit('update:city', val);
-    },
-    postal_code_(val) {
-      this.$emit('update:postal_code', val);
-    },
-    page() {
-      if (this.$el.getAttribute('data-current') === 'true') {
-        this.check();
-      }
     },
   },
 };
