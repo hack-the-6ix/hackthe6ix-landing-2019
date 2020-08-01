4a4f67ac279c390aa98921a5c34834bfec27a6df
Henry T <henry@henrytu.me>
2020-08-01 13:14:00 -0400

    Added new mlh fields to graphql mutation

--- src/graphql/dashboard.js
+++ src/graphql/dashboard.js
@@ -25,8 +25,8 @@ export const FETCH_QR = `
 `;
 
 export const RSVP = `
-    mutation rsvp($id: ID!, $attending: Boolean!, $casl_acceptance: Boolean, $resume_permission: Boolean, $mlh_acceptance: Boolean) {
-        rsvp(applicant_id: $id, attending: $attending, casl_acceptance: $casl_acceptance, resume_permission: $resume_permission, mlh_acceptance: $mlh_acceptance) {
+    mutation rsvp($id: ID!, $attending: Boolean!, $casl_acceptance: Boolean, $resume_permission: Boolean, $mlh_acceptance: Boolean, $mlh_privacy: Boolean, $mlh_messages: Boolean) {
+        rsvp(applicant_id: $id, attending: $attending, casl_acceptance: $casl_acceptance, resume_permission: $resume_permission, mlh_acceptance: $mlh_acceptance, mlh_privacy: $mlh_privacy, mlh_messages: $mlh_messages) {
             success
         }
     }
